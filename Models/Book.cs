13a9e0e310f7318952eb39460f3a20e0c447da07
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-03-09 14:16:16 -0700

    mission10 fnish bootstrap

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -10,13 +11,16 @@ namespace Books.Models
     public partial class Book
     {
         public int BookId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
         public string Publisher { get; set; }
         public string Isbn { get; set; }
         public string Classification { get; set; }
         public string Category { get; set; }
         public int PageCount { get; set; }
+        [Required]
         public double Price { get; set; }
     }
 }
